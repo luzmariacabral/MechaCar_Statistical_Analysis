74bc989ee8e5b6025e8191a78cb7234cd738b299
Luz Maria <luzmariacabral80@gmail.com>
2021-06-26 12:36:24 -0700

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,8 @@ total_summary <- suspension_table %>% summarize(mean(PSI),median(PSI),sd(PSI),va
 total_summary
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),sd(PSI),var(PSI))
 lot_summary
+
+#Deliverable 3 t-test on suspension coils
+
+t.test(subset(suspension_table, Manufacturing_Lot=='Lot1')$PSI,mu=1500) #compare sample v. population means
+t.test(subset(suspension_table, Manufacturing_Lot=='Lot2')$PSI,mu=1500)
